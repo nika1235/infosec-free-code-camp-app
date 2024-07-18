ee3ff865a4c042400de1e377337b67c1d6531e14
admin <admin@admins-MacBook-Air.local>
2024-07-18 11:04:41 +1000

    Update

---
+++ myApp.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+qpp.listen(PORT, () => {
+  console.log('Useful Programmer Info Security App Started on Port ${PORT}')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
